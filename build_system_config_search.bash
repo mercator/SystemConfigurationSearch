874c3ce0430913f67f6047cfe02cd9f972c7f469
Peter Spiller <peter@fontis.com.au>
2013-12-02 09:58:50 +1100

    Move from local to community code pool

--- build_system_config_search.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir var/build
-
-#OS X, prevent ._ files
-export COPYFILE_DISABLE=true
-
-tar -cvf var/build/System_Config_Search.tar app/design/adminhtml/default/default/images/alanstormdotcom  app/code/local/Alanstormdotcom/Systemsearch  app/etc/modules/Alanstormdotcom_Systemsearch.xml
